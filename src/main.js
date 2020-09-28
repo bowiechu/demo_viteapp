12254b1eedfb21f817e2e3a17cd9da924a6f8f2a
Bowie Chu <nyoijizai@outlook.com>
2020-09-28 10:56:00 +0800

    初始化提交,创建项目并引入 Ant Design

---
+++ src/main.js
@@ -0,0 +1,7 @@
+import { createApp } from 'vue';
+import Antd from 'ant-design-vue';
+import Axios from 'axios';
+import App from './App.vue';
+import 'ant-design-vue/dist/antd.css';
+
+createApp(App).use(Antd).mount('#app');
